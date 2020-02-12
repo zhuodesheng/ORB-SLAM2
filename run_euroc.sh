babae1981d7013fcc16ab8d92a7a6c049a60db7c
zhuodesheng <zhuodesheng@outlook.com>
2020-02-12 17:11:21 +0800

    add run.sh files

---
+++ run_euroc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./Examples/Monocular/mono_euroc Vocabulary/ORBvoc.txt Examples/Monocular/EuRoC.yaml /home/zds/SLAM-System/data/indoor-ls/mav0/cam0/data Examples/Monocular/EuRoC_TimeStamps/MH01.tx
---
+++ run_tum.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./Examples/Monocular/mono_tum Vocabulary/ORBvoc.txt Examples/Monocular/TUM3.yaml /home/zds/SLAM-System/data/down/rgbd_dataset_freiburg3_structure_texture_far
